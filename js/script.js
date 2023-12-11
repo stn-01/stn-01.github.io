9c6717a2386742eb637b7c6d6d64c15e9fd3e283
stn-01 <p17098807@gmail.com>
2023-12-11 16:48:03 +0300

    test mobile screens

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+
+document.querySelector("#Width-detector").innerText = window.innerWidth;
+document.querySelector("#Height-detector").innerText = window.innerHeight;
