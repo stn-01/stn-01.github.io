8e2895c0651284848f6336714a97ba8a7aba4bf6
stn-01 <p17098807@gmail.com>
2023-12-11 17:04:24 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,5 @@
 console.log(window.innerWidth);
 console.log(window.innerHeight);
 
-
 document.querySelector("#Width-detector").innerText = window.innerWidth;
 document.querySelector("#Height-detector").innerText = window.innerHeight;
